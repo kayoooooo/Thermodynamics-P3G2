c81f5c05701c06a2bae8428139403e2a94f75d2b
Kevin Yao <k.yao.kayoo@gmail.com>
2022-03-09 19:38:58 -0500

    tidying up

--- thermo.R
+++ thermo.R
@@ -17,10 +17,4 @@ dat$temp = dat$temp + 273.15
 LSRL = lm(dat$lvl ~ dat$temp)
 print(LSRL)
 summary(LSRL)
-print(cor(dat$lvl, dat$temp))
-b = cor(dat$lvl, dat$temp) * sd(dat$lvl) / sd(dat$temp)
-print(b)
-a = mean(dat$lvl) - b * mean(dat$temp)
-ggplot(dat, aes(x = temp, y = lvl)) + 
-  geom_abline(slope = b, intercept = a) + 
-  geom_point()
+print(cor(dat$lvl, dat$temp)^2)
